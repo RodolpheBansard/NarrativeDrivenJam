2d5614440df01d0e4e6c11577cf74f5b7371ac9d
Rodolphe <rodolphebansard@yahoo.com>
2021-05-01 15:40:23 +0200

    initial commit

---
+++ Assets/Scripts/CameraSequence.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CameraSequence : MonoBehaviour
+{
+    public List<Light2D> cameras;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cameras.Count > 1)
+        {
+            foreach (Light2D camera in cameras)
+            {
+                camera.gameObject.SetActive(false);
+            }
+            StartCoroutine(Sequencing());
+        }
+        else
+        {
+            StartCoroutine(Blink());
+        }
+        
+        
+    }
+
+    IEnumerator Sequencing()
+    {
+        while (true)
+        {
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                if (i == 0)
+                {
+                    cameras[cameras.Count - 1].gameObject.SetActive(false);
+                }
+                else
+                {
+                    cameras[i-1].gameObject.SetActive(false);
+                }
+
+                cameras[i].gameObject.SetActive(true);
+                yield return new WaitForSeconds(1);
+                cameras[i].gameObject.SetActive(false);
+
+                if (i == cameras.Count - 1)
+                {
+                    cameras[0].gameObject.SetActive(true);
+                }
+                else
+                {
+                    cameras[i+1].gameObject.SetActive(true);
+                }
+            }
+        }
+        
+    }
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            cameras[0].gameObject.SetActive(true);
+            yield return new WaitForSeconds(1);
+            cameras[0].gameObject.SetActive(false);
+            yield return new WaitForSeconds(1);
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 movement;
+
+    private bool canMove = false;
+
+
+
+    void Update()
+    {
+        if (canMove)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(movement == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        rb.velocity = movement*moveSpeed*Time.fixedDeltaTime;
+    }
+
+    public void drawingFinished()
+    {
+        canMove = true;
+    }
+
+    
+}
